a7d8dd0ef96020dfbb9d7f74af1541e6dd86b0b1
liauw <iiauwtzmin@gmail.com>
2026-05-28 15:51:05 +0800

    Generate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,11 @@
-export const cart =[];
+export const cart =[{
+  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity: 2
+  },{
+  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity: 10
+  }
+];
 
 export function updateCartQuantity() {
   let cartQuantity = 0;
@@ -10,7 +17,7 @@ export function updateCartQuantity() {
   document.querySelector('.cart-quantity').innerHTML = cartQuantity;
 }
 export function addToCart(productId, quantity) {
-  const matchingProduct = cart.find((item) => item.id === productId);
+  const matchingProduct = cart.find((item) => item.productId === productId);
 
   if (matchingProduct) {
     matchingProduct.quantity += quantity;
